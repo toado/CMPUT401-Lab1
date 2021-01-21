a3dacc839ce29740a3762628ff1d08f5a23d9f1e
Justin Tan <41741915+toado@users.noreply.github.com>
2021-01-20 20:16:55 -0700

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ print(requests.__version__)
 
 r = requests.get("https://www.google.com/")
 
-code = requests.get("https://raw.githubusercontent.com/VinnyLuu/CMPUT404Lab1/master/script.py")
+code = requests.get("https://raw.githubusercontent.com/toado/CMPUT401-Lab1/main/script.py")
 
 f = open("script.py", "w")
 f.write(code.text)
